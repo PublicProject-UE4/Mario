2774abc298af15202bab7e69f8ed09e4012c580c
Tom <798317108@qq.com>
2020-07-27 16:11:34 +0800

    Frist Commit

---
+++ Source/Mario/MarioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARIO_API AMarioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
